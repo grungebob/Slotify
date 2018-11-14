0a69a85a4c0b45d9a0baeb582a5211f99bf9d83c
Robert Chung <chungrob@gmail.com>
2018-11-13 20:14:39 -0500

    Album Class

--- album.php
+++ album.php
@@ -6,17 +6,36 @@
         header("Location: index.php");
     }
 
-    $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
-    $album = mysqli_fetch_array($albumQuery);
+    // $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
+    // $album = mysqli_fetch_array($albumQuery);
     
-    echo $album['title'] . "<br>";
+    // echo $album['title'] . "<br>";
 
-    $artistId = $album['artist'];
+    $album = new Album($con, $albumId);
 
-    $artist = new Artist($con, $artistId);
+    $artist = $album->getArtist();
 
-    echo $artist->getName();
+    // $artistId = $album['artist'];
+    // $artist = new Artist($con, $artistId);
+
+    // echo $album['title'] . "<br>";
+    // echo $album->getTitle() . "<br>";
+    // echo $artist->getName();
 ?>
+
+<div class = "entityInfo">
+    <div class="leftSection">
+        <img src="<?php echo $album->getArtworkPath(); ?>">
+    </div>
+
+    <div class="rightSection">
+        <h2><?php echo $album->getTitle();?></h2>
+        <p>By <?php echo $artist->getName(); ?></p>
+        <p><?php echo $album->getNumberOfSongs();?> songs</p>
+    </div>
+
+
+</div>
 
 
 
